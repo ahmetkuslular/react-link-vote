b9c205a50c507d1045d02a31f440099bd3aeec7d
ahmetkuslular <ahmetkuslular@gmail.com>
2019-11-11 18:13:13 +0300

    Updates

---
+++ precache-manifest.255c9ea80a06de907fe16d2ef90b5d29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649cb6cf391ecb7739966ddca3322f2f",
+    "url": "/react-link-vote/index.html"
+  },
+  {
+    "revision": "7ca1465ca54db0f4d0e9",
+    "url": "/react-link-vote/static/css/2.00129a64.chunk.css"
+  },
+  {
+    "revision": "d6322e9ed569d99c345f",
+    "url": "/react-link-vote/static/css/main.f1c19445.chunk.css"
+  },
+  {
+    "revision": "7ca1465ca54db0f4d0e9",
+    "url": "/react-link-vote/static/js/2.aad057f5.chunk.js"
+  },
+  {
+    "revision": "d6322e9ed569d99c345f",
+    "url": "/react-link-vote/static/js/main.6e0f18a5.chunk.js"
+  },
+  {
+    "revision": "c5acdfa9bb0d9fedf93b",
+    "url": "/react-link-vote/static/js/runtime-main.dccb5f0d.js"
+  },
+  {
+    "revision": "c6e794ca74880adcfca3e96230fe8458",
+    "url": "/react-link-vote/static/media/hepsiburada-logo.c6e794ca.png"
+  }
+]);
