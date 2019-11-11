7cc4973c4cd52c854fdc45c3b70135314f24019a
ahmetkuslular <ahmetkuslular@gmail.com>
2019-11-11 18:57:21 +0300

    Updates

--- precache-manifest.f9a49d0de827a77748f02b12f4b9291f.js
+++ precache-manifest.f9a49d0de827a77748f02b12f4b9291f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eb182386a4b2f9b3ebebd76e11a19073",
+    "revision": "6339060bd33b0a8a9ca10309d10a64f7",
     "url": "/react-link-vote/index.html"
   },
   {
-    "revision": "7ca1465ca54db0f4d0e9",
+    "revision": "4469179a74869fdaf87d",
     "url": "/react-link-vote/static/css/2.00129a64.chunk.css"
   },
   {
-    "revision": "737195aebc78f09bb43f",
+    "revision": "1332f7dbf689781ffa20",
     "url": "/react-link-vote/static/css/main.f1c19445.chunk.css"
   },
   {
-    "revision": "7ca1465ca54db0f4d0e9",
-    "url": "/react-link-vote/static/js/2.aad057f5.chunk.js"
+    "revision": "4469179a74869fdaf87d",
+    "url": "/react-link-vote/static/js/2.3ce78423.chunk.js"
   },
   {
-    "revision": "737195aebc78f09bb43f",
-    "url": "/react-link-vote/static/js/main.3cb7639f.chunk.js"
+    "revision": "1332f7dbf689781ffa20",
+    "url": "/react-link-vote/static/js/main.24218032.chunk.js"
   },
   {
     "revision": "c5acdfa9bb0d9fedf93b",
